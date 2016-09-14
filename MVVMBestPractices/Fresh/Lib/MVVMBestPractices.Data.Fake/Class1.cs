784783db96861cbca40d2e98fa8ea5a263428ded
udara.alwis <udara.alwis@tippstr.com>
2016-09-14 13:52:03 +0800

    First Commit! WOOT!

---
+++ MVVMBestPractices/Fresh/Lib/MVVMBestPractices.Data.Fake/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MVVMBestPractices.Data.Fake
+{
+    public class Class1
+    {
+    }
+}
---
+++ MVVMBestPractices/Implemented/MVVMBestPractices/MVVMBestPractices/Controls/ToDoListItem.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace MVVMBestPractices.Controls
+{
+    public partial class ToDoListItem : Grid
+    {
+        public ToDoListItem()
+        {
+            InitializeComponent();
+        }
+    }
+}
